e628da76bc37cf5c892baf3185b0f05c164f7973
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2023-09-19 00:48:06 +0200

    Update

--- keyboards/sofle/keymaps/loipesmas/config.h
+++ keyboards/sofle/keymaps/loipesmas/config.h
@@ -11,14 +11,11 @@ for more options.
 
 #define NO_MUSIC_MODE
 
-#define RGB_DI_PIN D3
+#define WS2812_DI_PIN D3
 #define RGBLED_NUM 12
 #define RGBLED_SPLIT \
     { 6, 6 }
 #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
