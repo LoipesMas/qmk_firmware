09bc40f9acd2844627e3fd28272313e7d953e0fb
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2025-08-09 14:33:45 +0200

    swap encoders

--- keyboards/sofle/keymaps/loipesmas/keymap.c
+++ keyboards/sofle/keymaps/loipesmas/keymap.c
@@ -371,15 +371,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    } else if (index == 1) {
-        if (clockwise) {
             tap_code(KC_WH_D);
         } else {
             tap_code(KC_WH_U);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     }
     return false;
